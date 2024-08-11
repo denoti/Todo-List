525173788a2243c91265c3cbf60605c24a2b6e14
denoti <otidennis98@gmail.com>
2024-08-11 19:20:34 +0300

    Bundling for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: './src/index.js',
 
   devServer: {
     static: './dist',
+    publicPath: '/Todo-List/',
   },
 
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/Todo-List/',
   },
   module: {
     rules: [
